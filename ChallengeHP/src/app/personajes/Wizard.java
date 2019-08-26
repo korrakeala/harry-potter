e569a0df35650ec0ab48cbb37df730617e2536cb
ecaroa <elianacarolinaa@gmail.com>
2019-08-26 13:00:27 -0300

    wizard

--- ChallengeHP/src/app/personajes/Wizard.java
+++ ChallengeHP/src/app/personajes/Wizard.java
@@ -3,6 +3,7 @@ package app.personajes;
 import java.util.ArrayList;
 import java.util.List;
 
+import app.artefactos.Artefacto;
 import app.hechizos.Hechizo;
 import app.interfaces.IHacerMagia;
 
@@ -11,12 +12,16 @@ import app.interfaces.IHacerMagia;
  */
 public class Wizard implements IHacerMagia {
 
+    private int poderInicial;
+    
+
     public List<Hechizo> hechizos = new ArrayList<Hechizo>();
 
     @Override
     public void atacar(Personaje enemigo, Hechizo hechizo) {
 
     }
+    public List<Artefacto> artefactos = new ArrayList<Artefacto>();
 
 
 }
