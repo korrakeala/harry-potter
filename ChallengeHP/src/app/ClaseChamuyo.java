78a53b991abe2a5bc5064cca325185be284bad8e
korrakeala <renata.mansilla@gmail.com>
2019-08-27 11:49:42 -0300

    agrego ClaseChamuyo con metodoChamuyo() usado en el Main

---
+++ ChallengeHP/src/app/ClaseChamuyo.java
@@ -0,0 +1,31 @@
+package app;
+
+import java.util.Scanner;
+
+/**
+ * ClaseChamuyo
+ */
+public class ClaseChamuyo {
+
+    public void metodoChamuyo(){
+
+        Scanner Teclado = new Scanner(System.in);
+            
+        int a, b, c, d;
+
+        System.out.println("INGRESE EL MULTIPLICANDO:");
+        a = Teclado.nextInt();
+        
+        System.out.println("INGRESE EL MULTIPLICADOR:");
+        b = Teclado.nextInt();
+
+        System.out.println("INGRESE EL SUSTRAENDO:");
+        c = Teclado.nextInt();
+
+        
+        d = ((a * b) - c);
+
+        System.out.println("EL RESULTADO DE LA OPERACION ES: "+ d);
+
+    }
+}
--- ChallengeHP/src/app/artefactos/Artefacto.java
+++ ChallengeHP/src/app/artefactos/Artefacto.java
@@ -11,4 +11,14 @@ public class Artefacto implements IEsMagico {
     public Poder poder;
     public double amplificadorDanio;
     public double amplificadorSalud;
+
+    @Override
+    public boolean esInvisibleAMuggles() {
+        return false;
+    }
+
+    @Override
+    public boolean esInvisible() {
+        return false;
+    }
 }
