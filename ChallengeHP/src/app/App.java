c6b5737c93085102b27d6cc39ddc0daad0fbfefd
korrakeala <renata.mansilla@gmail.com>
2019-08-27 12:34:31 -0300

    adios ClaseChamuyo.java y ClaseChamuyo.class

--- ChallengeHP/src/app/App.java
+++ ChallengeHP/src/app/App.java
@@ -3,8 +3,5 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        ClaseChamuyo cc = new ClaseChamuyo();
-
-        cc.metodoChamuyo();
     }
 }
