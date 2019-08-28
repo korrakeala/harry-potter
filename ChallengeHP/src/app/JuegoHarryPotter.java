756e6a125e7f0a666108ddca228074503b84c16a
PilarSalaberry <pilarsalaberry@gmail.com>
2019-08-28 14:13:58 -0300

    agrego personaje y mas

--- ChallengeHP/src/app/JuegoHarryPotter.java
+++ ChallengeHP/src/app/JuegoHarryPotter.java
@@ -3,6 +3,11 @@ package app;
 import java.util.ArrayList;
 import java.util.List;
 
+import app.artefactos.Artefacto;
+import app.artefactos.CapaInvisibilidad;
+import app.hechizos.Parseltongue;
+import app.hechizos.Poder;
+import app.personajes.Elfo;
 import app.personajes.Personaje;
 import app.personajes.Wizard;
 
@@ -11,21 +16,29 @@ import app.personajes.Wizard;
  */
 public class JuegoHarryPotter {
 
-    List<Personaje> personajes = new ArrayList<Personaje>();
+    public static List<Personaje> personajes = new ArrayList<Personaje>();
 
     public void inicializarJuego() {
 
     }
 
-    public static void agregarPersonajes(){
+    public static void agregarPersonajes() {
         Personaje p = new Wizard();
-        p.nombre = "Harry Potter";
-        p.salud = 100;
-        p.estaVivo = true;
-        p.edad = 30;
+        Wizard harry = (Wizard) p;
+        harry.nombre = "Harry Potter";
+        harry.salud = 100;
+        harry.estaVivo = true;
+        harry.edad = 30;
+        Poder parsel = new Parseltongue();
+        harry.setPoderInicial(parsel);
+        harry.escoba = new Escoba("Saeta de Fuego");
+        Artefacto capa = new CapaInvisibilidad();
+        harry.artefactos.add(capa);
 
 
+        JuegoHarryPotter.personajes.add(harry);
 
+    
     }
 
 }
